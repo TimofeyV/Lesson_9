5975f1fd71ed1a6efaa7a9472688d399de5b30c1
TimofeyV <vazhenin.t@gmail.com>
2023-05-24 09:33:03 +0300

    Update001

---
+++ 001/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 64: Задайте значение N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+int CreateNumber(string message) // Вывод сообщений и создание числа
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+string Recursia(int startNumber) // Вывод всех натуральных чисел от N до 1
+{
+    int endNumber = 1;
+    if(startNumber == endNumber) return startNumber.ToString();
+    if (startNumber > endNumber)
+    {
+        return (startNumber + " " + Recursia(startNumber-1));
+    }
+    return (startNumber + " " + Recursia(startNumber+1));
+}
+
+int N = CreateNumber("Введите число: ");
+Console.WriteLine(Recursia(N));
---
+++ 002/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int[] CreateArray(string message1, string message2) // Вывод сообщений и создание числа
+{
+    Console.Write(message1);
+    int numberN = Convert.ToInt32(Console.ReadLine());
+    Console.Write(message2);
+    int numberM = Convert.ToInt32(Console.ReadLine());
+    int[] arr = new int[2] {numberN, numberM};
+    return arr;
+
+}
+
+int Recursia(int startNumber, int endNumber) // Вывод суммы чисел от N до M
+{
+   if(startNumber == endNumber) return startNumber;
+   else
+   {
+        return startNumber += Recursia(startNumber + 1, endNumber);
+   }
+}
+
+
+int [] arr = CreateArray("Введите N: ", "Введите M: ");
+Console.WriteLine(Recursia(arr[0], arr[1]));
